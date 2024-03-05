914aee4194bde1f2dd9f06ae3cd6ec6cee17e4c2
Jeroen Paasen <jeroen.paasen@student.hu.nl>
2024-03-05 18:53:54 +0100

    Toevoegen functie change

--- src/jeroen.py
+++ src/jeroen.py
@@ -11,6 +11,11 @@ def display_message():
     message = "Groeten van Jeroen"
     print(message)
 
+def change():
+    roq = "Aanpassing is gedaan"
+    print(roq)
+
 if __name__ == "__main__":
     display_art()
     display_message()
+    change()
